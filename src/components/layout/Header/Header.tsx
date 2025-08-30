15c0d1a7968ef74e5985fd2992866b623b85e3a0
Gerrit Maatkamp <gerrit.maatkamp@gmail.com>
2025-08-30 12:08:07 -0700

    added links and removed things that are not currently needed

--- src/components/layout/Header/Header.tsx
+++ src/components/layout/Header/Header.tsx
@@ -1,28 +1,40 @@
 import "./Header.scss";
 
 export default function Header() {
+  const isHome =
+    typeof window !== "undefined" && window.location.pathname === "/";
   return (
     <header className="header">
-      <a href="/">
-        <img
-          src="/bsb_logo.png"
-          alt="Bite Size Bible Logo"
-          className="header-logo"
-        />
-      </a>
+      <div className="header-logo">
+        <a href="/">
+          <img
+            src="/bsb_logo.png"
+            alt="Bite Size Bible Logo"
+            className="header-logo-image"
+          />
+        </a>
+        <h2>Bite Size Bible</h2>
+      </div>
       <nav className="header-nav">
-        <a className="header-nav-item" href="/">
-          Home
-        </a>
-        <a className="header-nav-item" href="/blog">
+        {!isHome && (
+          <a className="header-nav-item" href="/">
+            Home
+          </a>
+        )}
+        {/* <a className="header-nav-item" href="/blog">
           Blog
+        </a> */}
+        <a
+          className="header-nav-item"
+          href="https://giving.myamplify.io/App/Form/c06cb37f-73d3-4f90-8c4d-22b762d3cfe9"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Give
         </a>
-        <a className="header-nav-item" href="/support">
-          Support
-        </a>
-        <a className="header-nav-item" href="/contact">
+        {/* <a className="header-nav-item" href="/contact">
           Contact
-        </a>
+        </a> */}
       </nav>
     </header>
   );
